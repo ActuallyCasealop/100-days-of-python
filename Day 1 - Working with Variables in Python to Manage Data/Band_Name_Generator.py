f0c436fd04d35eaf3e08f832b68ec3adbb2a6e91
ActuallyCasealop <carlo.morante02@gmail.com>
2023-10-29 07:13:29 +0800

    Files Uploaded.

---
+++ Day 1 - Working with Variables in Python to Manage Data/Band_Name_Generator.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+#2. Ask the user for the city that they grew up in.
+#3. Ask the user for the name of a pet.
+#4. Combine the name of their city and pet and show them their band name.
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
+
+
+print("Greetings! this is a band name generator.")
+user_city = input("begin by typing in the name of the city you grew up in.\n>>>: ")
+user_pet_name = input("what's the name of your pet?\n>>>: ")
+print("I'm thinking of: "+user_city + " " + user_pet_name + "! pretty cool huh?")
---
+++ Day 1 - Working with Variables in Python to Manage Data/readme.md
@@ -0,0 +1,12 @@
+# Day 1 summary
+
+Day 1 is the basics of the basics. tackling the topics about variables,
+how to store and read user inputs, string manipulation and whatnot.
+
+originally Dr. Angela recommends using replit but for the sake of
+documentation and also because I prefer using VS code I'll just go on
+and copy some of the instructions from there. its also only up to day 15
+where replit will be used anyway.
+
+I'll try to stick to her format as much as possible for authenticity sake
+as if I'm following along live, just like when starting out.
