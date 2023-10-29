447e2f878f54a763770297b465140c2e9ec5ee61
ActuallyCasealop <carlo.morante02@gmail.com>
2023-10-29 10:30:39 +0800

    Files Uploaded.

---
+++ Day 3 - Control Flow and Logical Operators/readme.md
@@ -0,0 +1,4 @@
+# Summary of day 3.
+
+Day 3 tackles control flow with if-statements/comparison operators and how to nest them along
+with using logical/conditional operators to make choices and differences on users input.
---
+++ Day 3 - Control Flow and Logical Operators/treasure_island.py
@@ -0,0 +1,37 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+user_input = input("After landing your on the shore-line of the treasure island, you see a cave and on the [left] and a hill on the [right]. where would you like to go first?\n>>>: ")
+if user_input.lower() == "right":
+    print("the hillside collapse after walking near it dragging you and your men to be burried and never to be seen again.")
+else:
+    user_input = input("After walking inside the cave, you see a massive door with two levers; one on each side. which one to pull? [left] or [right]?\n>>>: ")
+    if user_input.lower() == "left":
+        print("as you pull the lever half way, the cave suddenly started rumbling and a massive boulder dropped from above;\nburrying you and your men alive, never to be seen again.")
+    else:
+        print("You pulled the lever and the door opened up to heaps of treasure! you won!")
