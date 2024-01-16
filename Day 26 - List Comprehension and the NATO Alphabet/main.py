81c514f2bf02f475b56e52def1571192251f9609
ActuallyCasealop <carlo.morante02@gmail.com>
2024-01-16 18:28:34 +0800

    Final changes.

--- Day 26 - List Comprehension and the NATO Alphabet/main.py
+++ Day 26 - List Comprehension and the NATO Alphabet/main.py
@@ -1,27 +1,9 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+nato_data = pandas.read_csv(r"Day 26 - List Comprehension and the NATO Alphabet\nato_phonetic_alphabet.csv")
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+nato_dic = {val.letter:val.code for (key,val) in nato_data.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+user_input = input("Enter the word to be converted:\n>>>: ").upper()
+result = [nato_dic[i] for i in user_input]
+print(result)
