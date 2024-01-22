ac3aaa2ec580e815a002503678b1267236532375
ActuallyCasealop <carlo.morante02@gmail.com>
2024-01-22 19:19:18 +0800

    removed some space.

--- Day 27 - Tkinter, args, kwargs and Creating GUI Programs/main.py
+++ Day 27 - Tkinter, args, kwargs and Creating GUI Programs/main.py
@@ -3,10 +3,8 @@ import tkinter
 window = tkinter.Tk()
 window.title("First GUI")
 
-
 user_entry = tkinter.Entry(width=10)
 user_entry.grid(column=1,row=0,padx=5,pady=5)
-
 
 miles_label = tkinter.Label(text="Miles")
 miles_label.grid(column=2,row=0,padx=5,pady=5)
@@ -22,13 +20,8 @@ km_label.grid(column=2,row=1,padx=5,pady=5)
 
 def calculate():
     result_label.config(text=f"{round(float(user_entry.get())*1.609344)}")
-    
 
 calculate_button = tkinter.Button(text="Calculate",command=calculate)
 calculate_button.grid(column=1,row=2)
-
-
-
-
 
 window.mainloop()
