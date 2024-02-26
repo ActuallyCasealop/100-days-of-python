7de9dad3a31524fc4e20c7c8aeec05ef51db6670
Carlo Nazaire Morante <carlo.morante02@gmail.com>
2024-02-26 22:45:24 +0800

    Edited as part of Day 30 exercise.

--- Day 26 - List Comprehension and the NATO Alphabet/main.py
+++ Day 26 - List Comprehension and the NATO Alphabet/main.py
@@ -4,6 +4,15 @@ nato_data = pandas.read_csv(r"Day 26 - List Comprehension and the NATO Alphabet\
 
 nato_dic = {val.letter:val.code for (key,val) in nato_data.iterrows()}
 
-user_input = input("Enter the word to be converted:\n>>>: ").upper()
-result = [nato_dic[i] for i in user_input]
-print(result)
+def generate():
+    user_input = input("Enter the word to be converted:\n>>>: ").upper()
+    try:
+        result = [nato_dic[i] for i in user_input]
+    except KeyError:
+        print("Only string characters are allowed.")
+        generate()
+    else:
+        print(result)
+
+generate()
+
